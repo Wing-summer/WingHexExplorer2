35e01a4a779d9790ed7e69566ea97a1e6b4cf16b
wingsummer <1326224942@qq.com>
2024-07-14 23:08:48 +0800

    更新语言支持

--- src/class/languagemanager.cpp
+++ src/class/languagemanager.cpp
@@ -1,3 +1,31 @@
 #include "languagemanager.h"
 
-LanguageManager::LanguageManager() {}
+#include <QApplication>
+#include <QDir>
+#include <QLocale>
+
+LanguageManager &LanguageManager::instance() {
+    static LanguageManager ins;
+    return ins;
+}
+
+LanguageManager::LanguageManager() {
+    auto langPath =
+        qApp->applicationDirPath() + QDir::separator() + QStringLiteral("lang");
+
+    QDir langDir(langPath);
+    Q_ASSERT(langDir.exists());
+
+    auto langFiles =
+        langDir.entryInfoList({"ws_*.qm"}, QDir::Files | QDir::NoDotAndDotDot);
+    for (auto &langinfo : langFiles) {
+        auto langName = langinfo.fileName();
+        auto lang = langName.mid(3, langName.length() - 3 - 3);
+        QLocale locale(lang);
+        if (locale == QLocale::C) {
+            continue;
+        }
+        // TODO
+        m_langs << QLocale::countryToString(locale.country());
+    }
+}
